a69a7e83464665c3c1c45c3942a52749ab03d16e
mester890 <mester890@hotmail.com>
2017-02-15 13:08:42 +0100

    schedule 1.0

--- index.php
+++ index.php
@@ -43,5 +43,93 @@
 			</div>
 	</div>
 </div>
+
+
+	<div class="container">
+		<div class="schedule">
+			<table class="table table-bordered">
+				<tr class="header-row">
+					<th>Uke 5</th>
+					<th>Mandag</th>
+					<th>Tirsdag</th>
+					<th>Onsdag</th>
+					<th>Torsdag</th>
+					<th>Fredag</th>
+				</tr>
+				<tr>
+					<td class="left-row">1</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td class="left-row">2</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td class="left-row">3</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td class="left-row">4</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td class="left-row">5</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td class="left-row">6</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td class="left-row">7</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td class="left-row">8</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td class="left-row">9</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</table>
+		</div>		
+	</div>
 </body>
 </html>
