0c9aba9bdc877c62568a64a728f172dfccfa8992
mester890 <mester890@hotmail.com>
2017-02-15 17:37:00 +0100

    Added map instead of grey box, working.
    Floating left/right to check functionality.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Timeoversikt</title>
 	<!-- Latest compiled and minified CSS morten tester -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -36,22 +36,13 @@
 			</div>
 		</a>
 
-			<div class="btn" id="etg1" style="display:none;">
-<<<<<<< HEAD
+			<div class="btn" id="_etg1" style="display:none;">
 				<h3>ETG 1 - A</h3>	
 				<h3>ETG 1 - B</h3>
 			</div>
-			<div class="btn" id="etg2" style="display:none;">
+			<div class="btn" id="_etg2" style="display:none;">
 				<h3>ETG 2 - A</h3>	
 				<h3>ETG 2 - B</h3>
-=======
-				<a>ETG 1 - A</a>	
-				<a>ETG 1 - B</a>
-			</div>
-			<div class="btn" id="etg2" style="display:none;">
-				<a>ETG 2 - A</a>	
-				<a>ETG 2 - B</a>
->>>>>>> origin/master
 			</div>
 
 			<div class="klasseplan" id="klp1" 
@@ -62,65 +53,122 @@
 			style="display:none;">
 
 			</div>
-	</div>
-<div class="map_container">
-<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 559 1671">
-	<defs><style>.cls-1{fill:#fff;}</style></defs>
-	<title>b_floy</title>
-<a href="">
-	<rect class="cls-1" x="4" y="4" width="307" height="217"/>
-	<path d="M307,8V217H8V8H307m8-8H0V225H315V0Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="312" y="4" width="243" height="217"/>
-	<path d="M551,8V217H316V8H551m8-8H308V225H559V0Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="4" y="221" width="243" height="217"/>
-	<path d="M243,225V434H8V225H243m8-8H0V442H251V217Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="4" y="436" width="307" height="322"/>
-	<path d="M307,440V754H8V440H307m8-8H0V762H315V432Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="4" y="758" width="307" height="209"/>
-	<path d="M307,762V963H8V762H307m8-8H0V971H315V754Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="388" y="221" width="167" height="217"/>
-	<path d="M551,225V434H392V225H551m8-8H384V442H559V217Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="388" y="436" width="167" height="189"/>
-	<path d="M551,440V621H392V440H551m8-8H384V629H559V432Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="388" y="626" width="167" height="132"/>
-	<path d="M551,630V754H392V630H551m8-8H384V762H559V622Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="4" y="968" width="197" height="209"/>
-	<path d="M197,972v201H8V972H197m8-8H0v217H205V964Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="201" y="968" width="189" height="209"/>
-	<path d="M386,972v201H205V972H386m8-8H197v217H394V964Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="300" y="1362" width="255" height="305"/>
-	<path d="M551,1366v297H304V1366H551m8-8H296v313H559V1358Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="231" y="1362" width="68" height="305"/>
-	<path d="M295,1366v297H235V1366h60m8-8H227v313h76V1358Z"/>
-</a>
-<a href="">
-	<rect class="cls-1" x="4" y="1362" width="226" height="305"/>
-	<path d="M226,1366v297H8V1366H226m8-8H0v313H234V1358Z"/>
-</a>
-</svg>
+<div class="map_container" id="etg2" style="display: none; float: right">
+	<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 559 1671">
+		<defs><style>.cls-1{fill:#fff;}</style></defs>
+	<a href="">
+		<rect class="cls-1" x="4" y="4" width="307" height="217"/>
+		<path d="M307,8V217H8V8H307m8-8H0V225H315V0Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="312" y="4" width="243" height="217"/>
+		<path d="M551,8V217H316V8H551m8-8H308V225H559V0Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="221" width="243" height="217"/>
+		<path d="M243,225V434H8V225H243m8-8H0V442H251V217Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="436" width="307" height="322"/>
+		<path d="M307,440V754H8V440H307m8-8H0V762H315V432Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="758" width="307" height="209"/>
+		<path d="M307,762V963H8V762H307m8-8H0V971H315V754Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="388" y="221" width="167" height="217"/>
+		<path d="M551,225V434H392V225H551m8-8H384V442H559V217Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="388" y="436" width="167" height="189"/>
+		<path d="M551,440V621H392V440H551m8-8H384V629H559V432Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="388" y="626" width="167" height="132"/>
+		<path d="M551,630V754H392V630H551m8-8H384V762H559V622Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="968" width="197" height="209"/>
+		<path d="M197,972v201H8V972H197m8-8H0v217H205V964Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="201" y="968" width="189" height="209"/>
+		<path d="M386,972v201H205V972H386m8-8H197v217H394V964Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="300" y="1362" width="255" height="305"/>
+		<path d="M551,1366v297H304V1366H551m8-8H296v313H559V1358Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="231" y="1362" width="68" height="305"/>
+		<path d="M295,1366v297H235V1366h60m8-8H227v313h76V1358Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="1362" width="226" height="305"/>
+		<path d="M226,1366v297H8V1366H226m8-8H0v313H234V1358Z"/>
+	</a>
+	</svg>
 </div>
+<div class="map_container" id="etg1" style="display: none; float: left">
+	<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 559 1671">
+		<defs><style>.cls-1{fill:#fff;}</style></defs>
+	<a href="">
+		<rect class="cls-1" x="4" y="4" width="307" height="217"/>
+		<path d="M307,8V217H8V8H307m8-8H0V225H315V0Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="312" y="4" width="243" height="217"/>
+		<path d="M551,8V217H316V8H551m8-8H308V225H559V0Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="221" width="243" height="217"/>
+		<path d="M243,225V434H8V225H243m8-8H0V442H251V217Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="436" width="307" height="322"/>
+		<path d="M307,440V754H8V440H307m8-8H0V762H315V432Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="758" width="307" height="209"/>
+		<path d="M307,762V963H8V762H307m8-8H0V971H315V754Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="388" y="221" width="167" height="217"/>
+		<path d="M551,225V434H392V225H551m8-8H384V442H559V217Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="388" y="436" width="167" height="189"/>
+		<path d="M551,440V621H392V440H551m8-8H384V629H559V432Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="388" y="626" width="167" height="132"/>
+		<path d="M551,630V754H392V630H551m8-8H384V762H559V622Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="968" width="197" height="209"/>
+		<path d="M197,972v201H8V972H197m8-8H0v217H205V964Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="201" y="968" width="189" height="209"/>
+		<path d="M386,972v201H205V972H386m8-8H197v217H394V964Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="300" y="1362" width="255" height="305"/>
+		<path d="M551,1366v297H304V1366H551m8-8H296v313H559V1358Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="231" y="1362" width="68" height="305"/>
+		<path d="M295,1366v297H235V1366h60m8-8H227v313h76V1358Z"/>
+	</a>
+	<a href="">
+		<rect class="cls-1" x="4" y="1362" width="226" height="305"/>
+		<path d="M226,1366v297H8V1366H226m8-8H0v313H234V1358Z"/>
+	</a>
+	</svg>
+		</div>
+		</div>
+	</div>
 <!--<button id="btn1">ETG 1</button>
 <button id="btn2">ETG 2</button>
 -->
