d351046ac894acdb53d4bc2a6a4de00e16526586
mester890 <mester890@hotmail.com>
2017-02-19 12:26:20 +0100

    JSON table fill - working. Local file - API down.

--- test.php
+++ test.php
@@ -14,12 +14,11 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+<style>
+	/* Use CSS to style the elements as you like. And, put the CSS stylesheet in a different file. */
+</style>
+
 <h1>Parsing JSON - Teachers</h1>
-
-<ul id="demo"></ul> 
-
-<p id="testing"></p>
-
 <div class="container">
 <table class="table table-bordered">
 	<tr>
@@ -32,84 +31,93 @@
 	</tr>
 	<tr>
 		<td>1</td>
-		<td id="ma1" class="tbData"></td>
-		<td id="ti1" class="tbData"></td>
-		<td id="on1" class="tbData"></td>
-		<td id="to1" class="tbData"></td>
-		<td id="fr1" class="tbData"></td>
+		<td id="d1-1" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-1" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-1" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-1" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-1" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
 	</tr>
 	<tr>
 		<td>2</td>
-		<td id="ma2" class="tbData"></td>
-		<td id="ti2" class="tbData"></td>
-		<td id="on2" class="tbData"></td>
-		<td id="to2" class="tbData"></td>
-		<td id="fr2" class="tbData"></td>
+		<td id="d1-2" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-2" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-2" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-2" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-2" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
 	</tr>
 	<tr>
 		<td>3</td>
-		<td id="ma3" class="tbData"></td>
-		<td id="ti3" class="tbData"></td>
-		<td id="on3" class="tbData"></td>
-		<td id="to3" class="tbData"></td>
-		<td id="fr3" class="tbData"></td>
+		<td id="d1-3" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-3" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-3" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-3" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-3" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+	</tr>
+	<tr>
+		<td>4</td>
+		<td id="d1-4" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-4" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-4" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-4" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-4" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+	</tr>
+	<tr>
+		<td>5</td>
+		<td id="d1-5" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-5" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-5" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-5" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-5" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+	</tr>
+	<tr>
+		<td>6</td>
+		<td id="d1-6" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-6" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-6" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-6" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-6" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+	</tr>
+	<tr>
+		<td>7</td>
+		<td id="d1-7" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-7" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-7" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-7" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-7" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+	</tr>
+	<tr>
+		<td>8</td>
+		<td id="d1-8" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-8" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-8" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-8" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-8" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+	</tr>
+	<tr>
+		<td>9</td>
+		<td id="d1-9" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d2-9" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d3-9" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d4-9" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
+		<td id="d5-9" class="tbData"><div class="teacher-name"></div><div class="subject-name"></div></td>
 	</tr>
 </table>
 </div>
 <!-- jQuery :) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 
-<script>
-	
+<script>		
 	// this will load the data from the API. It is an async call, and when it is DONE...
-	$.get('http://enormt-api-test.azurewebsites.net/Api/Time?room=b205&week=3')	.done(function(data){
-		// ... this function will be called :D
-		//console.log(data); 
+	$.get('js/data.json')	.done(function(data){
 		
-		// Check it out!
-		for(var i = 0; i < data.length; i++) {
-			// iterate through the list of "Time"
+	
+		for(var i = 0; i < data.length; i++) {	
 			
-			// this will log the object to the console
-			//console.log(data[i]);
-						
-			// you can also access the fields of the object, like this: 
-			//console.log(data[i].Teacher);
 			var t = data[i];
-
-			var tDate = t.Date;
-
-			
-			var result = tDate.split("T");
-			var dateOnly = result[0];
-			var timeOnly = result[1].split(":00+")[0];
-			function getDayName(dateString) {
-			  return ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'][new Date(dateString).getDay()];
-			}
-
-			// Usage: Get the day name of a date
-			var day = getDayName(dateOnly);
-
-			if (timeOnly == '08:00' & day == "Mandag") {
-				document.getElementById("ma1").innerHTML = t.Subject + " " + t.Teacher;
-			} 
-			if (timeOnly == '08:00' & day == "Tirsdag") {
-				document.getElementById("ti1").innerHTML = t.Subject + " " + t.Teacher;
-			}
-			
-
-
-			// you can log multiple data if you like, at once!
-			//console.log(data[i].Teacher, data[i].Subject);
-			
-			// for simplicity, you can set the current 'Time' to a variable, and access it like this: 
-			
-
-			//console.log(t.Teacher, t.Subject); 
-
-			// then you don't have to write "data[i]" every time :)
-			
-			// live long and prosper
+			var cell = 'd' + t.Day + '-' + t.Time; 
+			var $ourCell = $('#' + cell); // $ because it is a jQuery object. 
+			$ourCell.find('.teacher-name').html(t.Teacher); // this will only set the data for the first hour, because the function 'getCorrectHour' does not return the 'correctHour' - yet :D
+			$ourCell.find('.subject-name').html(t.Subject);
 		}		
 	});
 
