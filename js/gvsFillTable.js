b592c79bddff517d41eb847f1e03d9059b7f2eee
mester890 <mester890@hotmail.com>
2017-02-20 00:07:26 +0100

    Here you go

--- js/gvsFillTable.js
+++ js/gvsFillTable.js
@@ -2,7 +2,7 @@
 $.get('js/data.json')	.done(function(data){
 	for(var i = 0; i < data.length; i++) {			
 		var t = data[i];
-		var cell = 'd' + t.Day + '-' + t.Time; 
+		var cell = t.Room + '-d' + t.Day + '-' + t.Time; 
 		var $ourCell = $('#' + cell); // $ because it is a jQuery object. 
 		$ourCell.find('.teacher-name').html(t.Teacher); // this will only set the data for the first hour, because the function 'getCorrectHour' does not return the 'correctHour' - yet :D
 		$ourCell.find('.subject-name').html(t.Subject);
