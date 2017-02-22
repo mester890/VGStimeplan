90cd340ff694b5eb2d5934dbedfae026feda48ea
mester890 <mester890@hotmail.com>
2017-02-22 20:42:23 +0100

    Fix Sverres commitfeil

---
+++ test.php
@@ -0,0 +1,17 @@
+			<?php include 'menu.php'; ?>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="mapContainer">
+						<div id="map1">
+							<script src="js/gvsLoad.js"></script>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--end of container-->
+	</div>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/gvsFillTable.js"></script>
+	</body>
+</html>
