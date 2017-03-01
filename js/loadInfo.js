a8e60d9ae8ee38afd490a49b9487e28ba69c6cb5
mester890 <mester890@hotmail.com>
2017-03-01 10:09:51 +0100

    Final Commit - presentation

--- js/loadInfo.js
+++ js/loadInfo.js
@@ -1,86 +1,54 @@
-function createLink(cellName, subject) {
-	$.get('js/subjectInfo.json')	.done(function(data){
-		for(var i = 0; i < data.length; i++) {			
-			var t = data[i];
-			var $myCell = $('#' + cellName);
-			if (subject == t.Name) {
-				$myCell.find('.subject-name').html('<a href="#" data-toggle="modal" data-target="#' + t.Name + '">' + t.Name + '</a>');
-			} else {
-				$myCell.find('subject-name').html(subject);
+// function createLink(cellName, subject) {
+// 	$.get('js/subjectInfo.json')	.done(function(data){
+// 		for(var i = 0; i < data.length; i++) {			
+// 			var t = data[i];
+// 			var $myCell = cellName;
+
+// 			if (subject == t.Name) {
+// 				$myCell.find('.subject-name').html('<a href="' + t.URL + '" target="_blank">' + t.Name + '</a>');
+// 				console.log(t.URL);
+// 			} else {
+// 				//$myCell.find('.subject-name').html(subject);
+// 			}
+// 		}
+// 	});
+// }
+
+
+$.get('http://enormt-api-test.azurewebsites.net/Api/Time?room=b201&week=3')	.done(function(data){
+	$.get('js/subjectInfo.json').done(function(subjects) {
+		var subjectFinder = function findEquals(s, f, $ourCell) {
+			 var foundIt = false; 
+			for (var i = 0; i < s.length; i++) {
+				var foundSubject = s[i].Name == f;
+				if(foundSubject) {
+					console.log(foundSubject, s[i], f);
+					$ourCell.find('.subject-name').html('<a href="' + s[i].URL + '" target="_blank">' + s[i].Name + '</a>');
+					foundIt = true;
+				}
 			}
+
+			if(!foundIt){
+				$ourCell.find('.subject-name').html(f);
+			}
+
+		}
+		for(var i = 0; i < data.length; i++) {			
+
+			var t = data[i];
+			var cell = 'd' + t.Day + '-' + t.Time; 
+			var $ourCell = $('#' + cell); // $ because it is a jQuery object.
+			if(t.Teacher == '' & t.Subject == '') {
+				$ourCell.css('background-color', 'rgba(0, 255, 0, 0.3)');	
+			} else {
+				subjectFinder(subjects, t.Subject, $ourCell);
+				$ourCell.find('.teacher-name').html(t.Teacher); 
+				$ourCell.css('background-color', 'rgba(255, 0, 0, 0.3)');
+			}
+			className = document.getElementById('className');
+			className.innerHTML = '3STA';	
 		}
 	});
-}
-
-$.get('js/subjectInfo.json')	.done(function(data){
-	for(var i = 0; i < data.length; i++){
-		var t = data[i]; 
-
-		// var modalButton = document.createElement('button');
-		// modalButton.setAttribute('type', 'button');
-		// modalButton.setAttribute('class', 'btn btn-primary btn-lg');
-		// modalButton.setAttribute('data-toggle', 'modal');
-		// modalButton.setAttribute('data-target', '#' + t.Name);
-		// modalButton.innerHTML = t.Name;
-		// document.body.appendChild(modalButton);
-
-		var modal = document.createElement('div');
-		modal.setAttribute('class', 'modal fade');
-		modal.setAttribute('id', t.Name);
-		modal.setAttribute('role', 'dialog');
-		modal_dialog = document.createElement('div');
-		modal_dialog.setAttribute('class', 'modal-dialog modal-lg');
-		modal_content = document.createElement('div');
-		modal_content.setAttribute('class', 'modal-content');
-		modal_header = document.createElement('div');
-		modal_header.setAttribute('class', 'modal-header')
-		button = document.createElement('button');
-		button.setAttribute('type', 'button');
-		button.setAttribute('class', 'close');
-		button.setAttribute('data-dismiss', 'modal');
-		button.innerHTML = "&times;";
-		modal_title = document.createElement('h4');
-		modal_title.setAttribute('class', 'modal-title');
-		modal_title.innerHTML = t.Name;
-		modal_body = document.createElement('div');
-		modal_body.setAttribute('class', 'modal-body');
-		modal_iframe = document.createElement('iframe');
-		modal_iframe.setAttribute('src', t.URL);
-		modal_iframe.setAttribute('width', '100%');
-		modal_iframe.setAttribute('height', '600px');
-
-		document.body.appendChild(modal);
-		modal.appendChild(modal_dialog);
-		modal_dialog.appendChild(modal_content);
-		modal_content.appendChild(modal_header);
-		modal_header.appendChild(button);
-		modal_header.appendChild(modal_title);
-		modal_content.appendChild(modal_body);
-		modal_body.appendChild(modal_iframe);
-	}
 });
 
-$.get('js/timeplan.json')	.done(function(data){
-	for(var i = 0; i < data.length; i++) {			
-		var t = data[i];
-		var cell = 'd' + t.Day + '-' + t.Time; 
-		var $ourCell = $('#' + cell); // $ because it is a jQuery object. 
-		createLink(cell, t.Subject);
 
-
-		if (t.CurrentClass == '3STA') {
-			$ourCell.find('.teacher-name').html(t.Teacher); 
-			//$ourCell.find('.subject-name').html(t.Subject);
-
-
-			$ourCell.find('.class-name').html((t.Room).toUpperCase());
-			$ourCell.css('background-color', 'rgba(255, 0, 0, 0.3)');
-		}
-		if (t.Subject == '') {
-			$ourCell.css('background-color', 'rgba(0, 255, 0, 0.3');
-			$ourCell.find('.subject-name').html('<h4 class="text-cetner">Fritime</h4>');
-		}
-		className = document.getElementById('className');
-		className.innerHTML = '3STA';	
-	}
-});
--- menu.php
+++ menu.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<title>Gauldal VGS - Romoversikt</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
@@ -75,7 +75,6 @@
 			</div>
 		  </div>
 
-<<<<<<< HEAD
 
 
 
@@ -96,24 +95,18 @@
 
 
 
-	      <script type="text/javascript">
-=======
-	      <script type="text/javascript">
-
-		 
-
->>>>>>> origin/master
-		    	function FindByAttributeValue(attribute, value)    {
-					  var All = document.getElementsByTagName('*');
-					  for (var i = 0; i < All.length; i++)       {
-					    if (All[i].getAttribute(attribute) == value) { return All[i]; }
-					  }
-					}
-			    $(document).ready(function(){
-			    	var url = document.location.href;
-			    	var refinedUrl = (url).split('/')[4];
-			    	var currentPage = FindByAttributeValue('href', refinedUrl);
-			    	console.log(currentPage);
-			    	currentPage.parentNode.setAttribute('class', 'active');
-					});
+	      	<script type="text/javascript">
+		   //  	function FindByAttributeValue(attribute, value)    {
+					//   var All = document.getElementsByTagName('*');
+					//   for (var i = 0; i < All.length; i++)       {
+					//     if (All[i].getAttribute(attribute) == value) { return All[i]; }
+					//   }
+					// }
+			  //   $(document).ready(function(){
+			  //   	var url = document.location.href;
+			  //   	var refinedUrl = (url).split('/')[4];
+			  //   	var currentPage = FindByAttributeValue('href', refinedUrl);
+			  //   	console.log(currentPage);
+			  //   	currentPage.parentNode.setAttribute('class', 'active');
+					// });
 		    </script>
